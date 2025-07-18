0cf61218585a40d117f351f4a5ffaf3455e28ef0
WESHOK <gdvarta@gmail.com>
2025-07-18 19:32:26 +0300

    db done almost

--- src/db/donor.py
+++ src/db/donor.py
@@ -1,28 +1,43 @@
-from sqlalchemy import BigInteger
-from sqlalchemy import VARCHAR
+from datetime import date
+from sqlalchemy import Date
+from sqlalchemy import Integer
+from sqlalchemy import Enum
+from sqlalchemy import VARCHAR, CHAR
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
 
 class Donor(Base):
-    """QuizUser model."""
+    """Donor model."""
 
-    __tablename__ = 'accounts'
+    __tablename__ = 'donors'
 
 
-    user_id: Mapped[int] = mapped_column(
-        BigInteger, primary_key=True, unique=True, nullable=False
-    )
     fio: Mapped[str] = mapped_column(
         VARCHAR(255), unique=False, nullable=True
     )
     group: Mapped[str] = mapped_column(
-        E, unique=False, nullable=True
+        Enum('Сотрудник', CHAR(7)), unique=False, nullable=True
     )
     gavrilov_amount: Mapped[int] = mapped_column(
-        Inte, unique=False, nullable=True
+        Integer, unique=False, nullable=True
     )
     fmba_amount: Mapped[int] = mapped_column(
-        E, unique=False, nullable=True
+        Integer, unique=False, nullable=True
+    )
+    sum: Mapped[int] = mapped_column(
+        Integer, unique=False, nullable=True
+    )
+    gavrilov_last_donation_date: Mapped[date] = mapped_column(
+        Date, unique=False, nullable=True
+    )
+    fmba_last_donation_date: Mapped[date] = mapped_column(
+        Date, unique=False, nullable=True
+    )
+    social_contacts: Mapped[str] = mapped_column(
+        VARCHAR(255), unique=False, nullable=True
+    )
+    number: Mapped[str] = mapped_column(
+        CHAR(12), unique=False, nullable=True
     )
